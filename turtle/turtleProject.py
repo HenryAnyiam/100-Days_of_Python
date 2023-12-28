0f70f195747713e49f07be894650630f89fbe767
HenryAnyiam <anyiamhenry7@gmail.com>
2023-12-28 13:30:39 +0100

    Turtle tries

---
+++ turtle/turtleProject.py
@@ -0,0 +1,86 @@
+from turtle import Screen, Turtle, colormode
+from random import choice, randint
+import colorgram
+
+turtle = Turtle()
+colormode(255)
+
+turtle.shape("turtle")
+turtle.color("red", "green")
+
+
+def draw_square(turtle):
+    for _ in range(4):
+        turtle.forward(100)
+        turtle.right(90)
+
+
+def draw_dashed_line(turtle):
+    for _ in range(15):
+        turtle.forward(10)
+        turtle.up()
+        turtle.forward(10)
+        turtle.down()
+
+
+def draw_various_shapes(turtle):
+    colors = ['red', 'green', 'blue',
+              'orange', 'yellow', 'aquamarine',
+              'pink', 'maroon']
+    for i in range(3, 11):
+        turn = int(360 / i)
+        turtle.pencolor(colors[i - 3])
+        for _ in range(i):
+            turtle.forward(100)
+            turtle.right(turn)
+
+
+def move_random(turtle):
+    turn = [turtle.right, turtle.left]
+    angle = [0, 90, 180, 360]
+    move = [turtle.forward, turtle.backward]
+    turtle.pensize(8)
+    turtle.speed(8)
+    for _ in range(200):
+        colors = (randint(0, 255), randint(0, 255), randint(0, 255))
+        turtle.pencolor(colors)
+        choice(turn)(choice(angle))
+        choice(move)(30)
+
+
+def draw_spirograph(turtle, gap):
+    turtle.speed(0)
+    loop = int(360 / gap)
+    for _ in range(loop):
+        colors = (randint(0, 255), randint(0, 255), randint(0, 255))
+        turtle.pencolor(colors)
+        turtle.circle(100)
+        turtle.left(gap)
+
+
+def draw_spot(turtle):
+    turtle.speed(0)
+    colors = colorgram.extract('image.jpeg', (18 * 32))
+    colors = [tuple(i.rgb) for i in colors]
+    turtle.penup()
+    x = -300
+    y = -270
+    turtle.setpos(x, y)
+    turtle.pendown()
+    for _ in range(30):
+        for _ in range(32):
+            turtle.pencolor(choice(colors))
+            turtle.dot(10)
+            turtle.penup()
+            turtle.forward(20)
+        y += 20
+        turtle.setpos(x, y)
+        turtle.pendown()
+
+
+draw_spot(turtle)
+
+
+myScreen = Screen()
+
+myScreen.exitonclick()
