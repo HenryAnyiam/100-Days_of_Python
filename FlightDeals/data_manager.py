6d7702cb7dad3864d088e66ac8737d91da5bed5a
HenryAnyiam <anyiamhenry7@gmail.com>
2024-01-21 17:30:57 +0100

    added data manager and flight search functionality

---
+++ FlightDeals/data_manager.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Data Manager Module"""
+
+import requests
+from typing import List, Dict
+from os import getenv
+
+class DataManager:
+    """Handle syncing with google sheets"""
+
+    def __init__(self) -> None:
+        """initialize DataMangaer Class"""
+        self.__data = self.get_data()
+        self.__f_data = {i['iataCode']: i for i in self.__data}
+        self.codes = [i['iataCode'] for i in self.__data]
+        self.__header = {
+            "Authorization": f"Bearer {getenv('AUTH')}"
+        }
+
+    def get_data(self) -> List[Dict]:
+        """get data from google sheets"""
+        response = requests.get(url="https://api.sheety.co/"
+                                "fc899fc681b980ccca8049dea1cfcd63"
+                                "/flightDeals/prices",
+                                headers=self.__header)
+        return response.json()['prices']
+    
+    
+    def compare_prices(self, flights: Dict = {}) -> None:
+        """compare prices and update if need be"""
+        self.updated = {}
+        for i in flights:
+            data = self.__f_data[i]
+            if flights[i] < data['lowestPrice']:
+                self.update_price(data['id'], flights[i])
+                self.updated[data['city']] = flights[i]
+        return self.updated
+    
+    def update_price(self, id: int, price: int) -> None:
+        """update price according to id"""
+        body = {
+            'price': {
+                'lowestPrice': price
+            }
+        }
+        requests.put(url="https://api.sheety.co/"
+                         "fc899fc681b980ccca8049dea1cfcd63"
+                         f"/flightDeals/prices/{id}",
+                         json=body,
+                         headers=self.__header)
---
+++ FlightDeals/flight_search.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Flight Search Module"""
+
+from typing import List, Dict
+import requests
+from os import getenv
+
+class FlightSearch:
+    """search for flight data"""
+
+    def __init__(self, city: str = "LON",
+                 flights: List = [],
+                 dates: List[str] = ["03/04/2024",
+                                     "17/04/2024"]) -> None:
+        """Initialize FlightSearch Module"""
+        self.flights = flights
+        self.city = city
+        self.dates = dates
+        self.__api_key = getenv("APIKEY")
+        self.__header = {
+            "apikey": self.__api_key,
+            "Content-Type": "applications/json"
+        }
+        self.searched_flights = {}
+        self.search_flights()
+    
+    def search_flights(self):
+        """search cheapest flights"""
+        for i in self.flights:
+            params = {
+                "fly_from": f"city:{self.city}",
+                "fly_to": f"airport:{i}",
+                "date_from": self.dates[0],
+                "date_to": self.dates[1],
+                "one_for_city": 1,
+                "max_stopovers": 0,
+                "curr": "GBP"
+            }
+            response = requests.get(url="https://api.tequila.kiwi.com/v2/search",
+                        headers=self.__header,
+                        params=params)
+            if response.status_code == 200:
+                self.searched_flights[i] = response.json()["data"][0]["price"]
