d6180731388540deba937ea0dcc064221f57a94f
HenryAnyiam <anyiamhenry7@gmail.com>
2024-06-08 05:16:41 +0100

    Add view for user registration

--- Django/Whatsapp_Clone/server/base/serializers.py
+++ Django/Whatsapp_Clone/server/base/serializers.py
@@ -7,7 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     def create(self, validated_data):
-        user = get_user_model().objects.create_user(
+        user = get_user_model().object.create_user(
             email = validated_data['email'],
             password = validated_data['password'],
             first_name = validated_data.get('first_name', ''),
