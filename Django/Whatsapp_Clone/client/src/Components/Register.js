2af85a5bd8222d5d45b6db66d1e9c1e2906a19b8
HenryAnyiam <anyiamhenry7@gmail.com>
2024-06-08 11:10:21 +0100

    Add signup from client side

--- Django/Whatsapp_Clone/client/src/Components/Register.js
+++ Django/Whatsapp_Clone/client/src/Components/Register.js
@@ -1,23 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TextField, Button } from '@mui/material'
 
 export default function Register() {
+    const BASE_URL = "http://127.0.0.1:8000/";
+    const [formData, setFormData] = useState({
+        "email": "",
+        "first_name": "",
+        "last_name": "",
+        "password": ""
+    })
+    function handleFormSubmit() {
+        fetch(`${BASE_URL}register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData)
+        })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {console.log(data)})
+        .catch((err) => {console.log(err)})
+    }
     return (
         <div className='container text-center'>
             <div className='mt-3'>
-                <TextField id="email" type="email" label="Email" variant="outlined" />
+                <TextField id="email" type="email" label="Email" variant="outlined" onChange={(e) => {
+                    setFormData({...formData, email: e.target.value})
+                }}/>
             </div>
             <div className='mt-3'>
-                <TextField id="first_name" type="text" label="First Name" variant="outlined" />
+                <TextField id="first_name" type="text" label="First Name" variant="outlined" onChange={(e) => {
+                    setFormData({...formData, first_name: e.target.value})
+                }}/>
             </div>
             <div className='mt-3'>
-                <TextField id="last_name" type="text" label="Last Name" variant="outlined" />
+                <TextField id="last_name" type="text" label="Last Name" variant="outlined" onChange={(e) => {
+                    setFormData({...formData, last_name: e.target.value})
+                }}/>
             </div>
             <div className='mt-3'>
-                <TextField id="password" type="password" label="Password" variant="outlined" />
+                <TextField id="password" type="password" label="Password" variant="outlined" onChange={(e) => {
+                    setFormData({...formData, password: e.target.value})
+                }}/>
             </div>
             <div className='mt-3'>
-                <Button variant="contained">Submit</Button>
+                <Button variant="contained" onClick={handleFormSubmit}>Submit</Button>
             </div>
         </div>
     )
--- Django/Whatsapp_Clone/server/server/settings.py
+++ Django/Whatsapp_Clone/server/server/settings.py
@@ -31,7 +31,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -41,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'base',
 ]
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -56,6 +57,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'server.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+]
 
 TEMPLATES = [
     {
